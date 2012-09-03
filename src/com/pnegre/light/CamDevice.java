ac7d87d3dccfd09b3ea5fdf49ebde84554e00dfc
Pere Negre <pere.negre@gmail.com>
2012-09-04 01:09:56 +0200

    Anem fent

---
+++ src/com/pnegre/light/CamDevice.java
@@ -0,0 +1,69 @@
+package com.pnegre.light;
+
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * User: pnegre
+ * Date: 04/09/12
+ * Time: 01:07
+ */
+
+class CamDevice implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    boolean flashing = false;
+
+    CamDevice(SurfaceView sv) {
+        surfaceHolder = sv.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    void ledOn() {
+        try {
+            if ((flashing) || (camera != null)) {
+                return;
+            }
+            camera = Camera.open();
+            camera.setPreviewDisplay(surfaceHolder);
+            Camera.Parameters params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            flashing = true;
+        } catch (IOException e) { };
+    }
+
+    void ledOff() {
+        if ((!flashing) || (camera == null)) {
+            return;
+        }
+        flashing = false;
+        Camera.Parameters params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(params);
+
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
--- src/com/pnegre/light/MainActivity.java
+++ src/com/pnegre/light/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setOnTorch();
+                camDevice.ledOn();
             }
         });
 
@@ -36,7 +36,7 @@ public class MainActivity extends Activity
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setOffTorch();
+                camDevice.ledOff();
             }
         });
 
@@ -53,69 +53,5 @@ public class MainActivity extends Activity
         camDevice.ledOff();
         super.onPause();
     }
-
-    private void setOffTorch() {
-        camDevice.ledOff();
-    }
-
-    private void setOnTorch() {
-        camDevice.ledOn();
-    }
 }
 
-
-class CamDevice implements SurfaceHolder.Callback {
-
-    private SurfaceHolder surfaceHolder;
-    private Camera camera;
-    boolean flashing = false;
-
-    CamDevice(SurfaceView sv) {
-        surfaceHolder = sv.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-
-    void ledOn() {
-        try {
-            if ((flashing) || (camera != null)) {
-                return;
-            }
-            camera = Camera.open();
-            camera.setPreviewDisplay(surfaceHolder);
-            Camera.Parameters params = camera.getParameters();
-            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(params);
-            camera.startPreview();
-            flashing = true;
-        } catch (IOException e) { };
-    }
-
-    void ledOff() {
-        if ((!flashing) || (camera == null)) {
-            return;
-        }
-        flashing = false;
-        Camera.Parameters params = camera.getParameters();
-        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-        camera.setParameters(params);
-
-        camera.stopPreview();
-        camera.release();
-        camera = null;
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-}
